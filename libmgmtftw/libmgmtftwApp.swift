a8ae0b80573de0d1d1f338f91ee3ca150d284f7c
mkswagger <mathangykrishna16@gmail.com>
2024-04-22 09:49:51 +0530

    Initial Commit

---
+++ libmgmtftw/libmgmtftwApp.swift
@@ -0,0 +1,17 @@
+//
+//  libmgmtftwApp.swift
+//  libmgmtftw
+//
+//  Created by mathangy on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct libmgmtftwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
