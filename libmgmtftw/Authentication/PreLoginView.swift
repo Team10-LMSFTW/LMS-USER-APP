cd5bbffd1e5b25a78efc43da336d2e449a837a20
kg4687 <kg4687@srmist.edu.in>
2024-04-24 12:37:22 +0530

    Removed the Offsets to make the ui.

--- libmgmtftw/Authentication/PreLoginView.swift
+++ libmgmtftw/Authentication/PreLoginView.swift
@@ -9,50 +9,72 @@ import SwiftUI
 
 struct PreLoginView: View {
     var body: some View {
+    
         ZStack {
             Image("backgroundImage") // Add your image name here
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
             VStack{
-                Text("Manage Your \nShelf with \nLibriLand")
-                    .font(.custom("SF Pro Display", size: 35).bold())
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.leading)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    .lineSpacing(8)
-                    .offset(x:0,y:160)
-                
-                Button(action: {
-                    // Add your login action here
-                }) {
-                    Text("Login                                    ")
-                        .font(Font.custom("SF Pro Display", size: 20).bold()) // Use SF Pro font with size 18 and make it bold
+                Spacer()
+                Spacer()
+                VStack{
+                    Spacer()
+                    Spacer()
+                    ZStack{
                         
-                        .foregroundColor(.white) // Make text color white
-                        .padding() // Add padding around the text
-                        .background(Color(hex: "503E88"))
+                        VStack {
+                            Text("Manage Your \nShelf with \nLibriLand")
+                                .font(.custom("SF Pro Display", size: 35).bold())
+                                .fontWeight(.bold)
+                                .multilineTextAlignment(.leading)
+                                .foregroundColor(.white)
+                                .multilineTextAlignment(.center)
+                            .lineSpacing(8)
+                        }.padding(.top,160)
+                            
                         
-                }
-                .background(Color(hex: "503E88"))
-                .cornerRadius(30)
-                .offset(y:180)
-                Divider()
-                    .background(Color.white)
-                    .frame(maxWidth: 280)
-                    .offset(y:190)
-                HStack {
-                                    Text("Don't have an account? ")
-                                        .foregroundColor(.white)
-                                    
-                                    Text("SignUp")
-                                        .foregroundColor(.white)
-                                        .underline()
-                                        
-                            }
-                .offset(y:200)
+                        
+                        Button(action: {
+                            // Add your login action here
+                        }) {
+                            Text("Login                                    ")
+                                .font(Font.custom("SF Pro Display", size: 20).bold()) // Use SF Pro font with size 18 and make it bold
+                            
+                                .foregroundColor(.white) // Make text color white
+                                .padding() // Add padding around the text
+                                .background(Color(hex: "503E88"))
+                            
                         }
+                        .background(Color(hex: "503E88"))
+                        .cornerRadius(30)
+                        .padding(.top,470)
+                        
+                        Divider()
+                            .background(Color.white)
+                            .frame(maxWidth: 280)
+                            .padding(.top,550)
+                        
+                        HStack {
+                            Text("Don't have an account? ")
+                                .foregroundColor(.white)
+                                .padding(.top,580)
+                            
+                            Text("SignUp")
+                                .foregroundColor(.white)
+                                .underline()
+                                .padding(.top,580)
+                            
+                        }
+                        
+                        
+                    }
+                    
+                    Spacer()
+
+                }
+                Spacer()
+            }
                     }
             }
         }
