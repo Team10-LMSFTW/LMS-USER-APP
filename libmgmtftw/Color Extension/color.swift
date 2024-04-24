54c63663725f850820a325e0e154cf903aaf9c22
anvit18 <av7720@srmist.edu.in>
2024-04-24 17:47:07 +0530

    added onboardingview :tada:

---
+++ libmgmtftw/Color Extension/color.swift
@@ -0,0 +1,23 @@
+//
+//  color.swift
+//  libmgmtftw
+//
+//  Created by Anvit Pawar on 24/04/24.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgbValue & 0x0000FF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue)
+    }
+}
